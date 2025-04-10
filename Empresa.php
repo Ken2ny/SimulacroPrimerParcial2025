a968117307fe5331e8ded2f423eebff269fff428
Ken2ny <KenOficial10@hotmail.com>
2025-04-10 15:56:23 -0300

    d

---
+++ Empresa.php
@@ -0,0 +1,58 @@
+<?php
+
+class Empresa{
+
+    private $denominacion;
+    private $direccion;
+    private $coleccionCliente;
+    private $coleccionMoto;
+    private $ventas;
+
+    public function __construct($denominacion,$direccion,$coleccionCliente,$coleccionMoto, $ventas){
+        $this->denominacion = $denominacion;
+        $this->direccion = $direccion;
+        $this->coleccionCliente = $coleccionCliente;
+        $this->coleccionMoto = $coleccionMoto;
+        $this->ventas = $ventas;
+    }
+    //SET
+    public function setDenominacion($denominacion){
+        $this->direccion = $denominacion;
+    }
+    public function setDireccion($direccion){
+        $this->direccion = $direccion;
+    }
+    public function setColeccionCliente($coleccionCliente){
+        $this->coleccionCliente = $coleccionCliente;
+    }
+    public function setColeccionMoto($coleccionMoto){
+        $this->coleccionMoto = $coleccionMoto;
+    }
+    public function setVentas($ventas){
+        $this->ventas = $ventas;
+    }
+    //GET
+    public function get(){
+        return $this->
+    }
+    public function get(){
+        return $this->
+    }
+    public function get(){
+        return $this->
+    }
+    public function get(){
+        return $this->
+    }
+    public function get(){
+        return $this->
+    }
+
+
+
+
+
+
+
+
+}
--- TestSimulacro.php
+++ TestSimulacro.php
@@ -3,7 +3,4 @@
 require_once "Cliente.php";
 require_once "Moto.php";
 require_once "Venta.php";
-
-
-$cliente = new Cliente("Lucas", "Hergerberg", "activo","Masculino", "34223456");
-echo $cliente;
+require_once "Empresa.php";
