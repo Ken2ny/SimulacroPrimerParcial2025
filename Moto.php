a0bd3c9aa5a0892caa1a7ff12bfd44f69e337b38
Ken2ny <KenOficial10@hotmail.com>
2025-04-10 14:59:47 -0300

    a

---
+++ Moto.php
@@ -0,0 +1,32 @@
+<?php
+
+class Moto{
+
+    private $codigo;
+    private $costo;
+    private $anhoFabricacion;
+    private $descripcion;
+    private $porcentajeAnual;
+    private $activa;
+
+    public function __construct($codigo, $costo, $anhoFabricacion, $descripcion, $porcentajeAnual, $activa){
+        $this->codigo = $codigo;
+        $this->costo = $costo;
+        $this->anhoFabricacion = $anhoFabricacion;
+        $this->descripcion = $descripcion;
+        $this->porcentajeAnual = $porcentajeAnual;
+        $this->activa = $activa;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ TestSimulacro.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "Cliente.php";
+require_once "Moto.php";
+
+
+$cliente = new Cliente("Lucas", "Hergerberg", "activo","Masculino", "34223456");
+echo $cliente;
