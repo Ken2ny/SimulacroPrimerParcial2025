c5a4bf82db460a23534a68902e5f6a620fe5ba36
Ken2ny <KenOficial10@hotmail.com>
2025-04-11 13:09:55 -0300

    Update TestEmpresa.php

--- TestEmpresa.php
+++ TestEmpresa.php
@@ -17,6 +17,7 @@ $moto1 = new Moto(11, "2230000", 2022, "Benelli Imperiale 400", 85,"activa");
 $moto2 = new Moto(12, "584000", 2021, "Zanella Zr 150 Ohc", 70,"activa");
 $moto3 = new Moto(13, "999900", 2023, "Zanella Patagonian Eagle 250", 55,"falso");
 
+echo "//////////////////////////\n";
 echo "Lista de motos: \n";
 echo "Moto 1: " . $moto1->getDescripcion() . "\n";
 echo "Moto 2: " . $moto2->getDescripcion() . "\n";
@@ -28,6 +29,7 @@ $precioVenta1 = $empresa->registrarVenta([11, 12, 13], $objcliente2);
 $precioVenta2 = $empresa->registrarVenta([0], $objcliente2);
 $precioVenta3 = $empresa->registrarVenta([2], $objcliente2);
 
+echo "////////////////////////////\n";
 echo "Precio Venta 1: " . $precioVenta1 . "\n";
 echo "Precio Venta 2: " . $precioVenta2 . "\n";
 echo "Precio Venta 3: " . $precioVenta3 . "\n";
